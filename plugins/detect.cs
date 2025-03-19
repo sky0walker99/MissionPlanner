20ee165bd0bf1e8de1319e52d35b5a142ab3303b
sky0walker99 <haroonbmc0@gmail.com>
2025-03-19 17:57:36 +0530

    Update EmguCV

--- plugins/detect.cs
+++ plugins/detect.cs
@@ -3,3 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using MissionPlanner;
 using MissionPlanner.GCSViews;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.CvEnum;
+
+namespace QRCodeDetection{
+    public class Detection :  MissionPlanner.Plugin.Plugin
+    {
+        
+    }
+}
+    
