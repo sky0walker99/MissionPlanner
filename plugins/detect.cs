108af7a9f1afad7035e07a610e0f16a369551008
Haroon <106879583+sky0walker99@users.noreply.github.com>
2025-03-20 00:11:15 +0530

    UPdate

--- plugins/detect.cs
+++ plugins/detect.cs
@@ -6,11 +6,29 @@ using MissionPlanner.GCSViews;
 using Emgu.CV;
 using Emgu.CV.Structure;
 using Emgu.CV.CvEnum;
+using Emgu.CV.Util;
+using Emgu.CV.Barcode;
 
 namespace QRCodeDetection{
-    public class Detection :  MissionPlanner.Plugin.Plugin
+    public class QRDetection :  MissionPlanner.Plugin.Plugin
     {
-        
+        public override string Name => "QR Code Detector";
+        public override string Version => "1.0";
+        public override string Author => "Your Name";
+
+        private VideoCapture capture; // EmguCV Class for VideoCapture
+        private PictureBox displayBox;
+        private BarcodeDetector detector; // EmguCV Class for QRDetection
+        private Thread captureThread;
+    public QRDetection(){
+    capture = new VideoCapture;
+    displayBox = new PictureBox{
+
+            SizeMode = PictureBoxSizeMode.StretchImage,
+            Dock = DockStyle.Fill
+    }
+
+    }
     }
 }
     
